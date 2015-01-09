e0cac3e7587e8b0d6a2a81d6d8572ac9f1377493
Dale Chetney <dalechetney@gmail.com>
2015-01-09 10:56:23 -0700

    Villager Movement

--- DragonUnleashed/Assets/Scripts/NetworkAgent.cs
+++ DragonUnleashed/Assets/Scripts/NetworkAgent.cs
@@ -3,10 +3,37 @@ using System.Collections;
 
 public class NetworkAgent : MonoBehaviour {
 
+    private Vector3 networkPosition;
+    private Quaternion networkRotation;
+
 	// Use this for initialization
 	void Start () {
 	
 	}
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else
+        {
+            networkPosition = (Vector3)stream.ReceiveNext();
+            networkRotation = (Quaternion)stream.ReceiveNext();
+        }
+    }
+
+    public Vector3 GetNetworkPosition()
+    {
+        return networkPosition;
+    }
+
+    public Quaternion GetNetworkRotation()
+    {
+        return networkRotation;
+    }
 	
 	// Update is called once per frame
 	void Update () {
