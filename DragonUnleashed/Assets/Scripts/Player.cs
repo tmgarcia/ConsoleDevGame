6571399e089e04dbe47147c545f9adad9fa6de3e
Jeremy Jones <jeremy29229@gmail.com>
2015-01-11 04:17:28 -0700

    respawn script added

---
+++ DragonUnleashed/Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class Player : MonoBehaviour
+{
+	public PlayerRole Role = PlayerRole.Villager;
+	private Flammable flammable;
+
+	[HideInInspector]
+	public bool IsAlive;
+
+
+	// Use this for initialization
+	void Start()
+	{
+		flammable = GetComponent<Flammable>();
+
+		if (flammable == null)
+		{
+			Debug.LogException(new UnityException("Player GameObject must contain Flammable script."));
+			Destroy(this);
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (flammable.Integrity <= float.Epsilon && IsAlive)
+		{
+			OnDeath();
+		}
+	}
+
+	public void OnDeath()
+	{
+		if (Role == PlayerRole.Villager)
+		{
+			RespawnManager.instance.Respawn(gameObject);
+			flammable.Integrity = flammable.StartingIntegrity;
+		}
+		else if (Role == PlayerRole.Dragon)
+		{
+			print("You killed the dragon yay.");
+			IsAlive = false;
+		}
+	}
+}
---
+++ DragonUnleashed/Assets/Scripts/RespawnManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class RespawnManager : MonoBehaviour
+{
+	public static RespawnManager instance;
+	public List<GameObject> RespawnPoints;
+
+	void Start()
+	{
+		if (instance == null)
+		{
+			instance = this;
+		}
+	}
+
+	void Update()
+	{
+		Respawn(gameObject);
+	}
+
+	public void Respawn(GameObject character)
+	{
+		if (RespawnPoints.Count < 1)
+		{
+			Debug.LogException(new UnityException("Attempting to Respawn without any RespawnPoints!"));
+		}
+		else
+		{
+			character.transform.position = RespawnPoints[Random.Range(0, RespawnPoints.Count)].transform.position;
+		}
+	}
+}
