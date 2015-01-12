0127c4c2fc06f3fd514be5b41cfc7482e54058fe
Taylor Garcia <tclustuff@gmail.com>
2015-01-12 09:46:58 -0700

    ug

--- DragonUnleashed/Assets/Scripts/Archery.cs
+++ DragonUnleashed/Assets/Scripts/Archery.cs
@@ -44,15 +44,22 @@ public class Archery : Photon.MonoBehaviour
     public static void FireArrow(int playerId, Vector3 force)
     {
         photonView.RPC("RPCFireArrow", PhotonTargets.All, new object[] { playerId, force });
+        print("FireArrow " + playerId);
     }
 
 	[RPC]
 	private void RPCFireArrow(int firingPlayerID, Vector3 force)
 	{
+        print("RPCFireArrow " + firingPlayerID);
         if (na.playerID == firingPlayerID)
         {
+            print("correct player firing");
             GameObject launchedArrow = Instantiate(arrow, gameObject.transform.position + new Vector3(0.75f, 0.75f, 0), transform.rotation) as GameObject;
             launchedArrow.rigidbody.AddForce(force);
+        }
+        else
+        {
+            print("not correct player firing");
         }
 	}
 
