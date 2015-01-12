60197ee343e99e4638bfe4cf4d4c8e51cfce7e63
Dale Chetney <dalechetney@gmail.com>
2015-01-12 09:12:27 -0700

    no dragon twitch maybe

--- DragonUnleashed/Assets/Scripts/DragonSegment.cs
+++ DragonUnleashed/Assets/Scripts/DragonSegment.cs
@@ -16,10 +16,17 @@ public class DragonSegment : MonoBehaviour {
     void Update()
     {
         if (movement == null) movement = GameObject.Find("DragonHead").GetComponent<DragonMovement>();
+        if (movement.isLocal)
+        {
             if (Vector3.Distance(transform.position, parent.position) > 3.5f)
             {
                 transform.position = Vector3.Lerp(transform.position, parent.position, Time.deltaTime * movement.speed);
             }
-        
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, GetComponent<NetworkAgent>().GetNetworkPosition(), Time.deltaTime * 5);
+            transform.rotation = Quaternion.Lerp(transform.rotation, GetComponent<NetworkAgent>().GetNetworkRotation(), Time.deltaTime * 5);
+        }
     }
 }
