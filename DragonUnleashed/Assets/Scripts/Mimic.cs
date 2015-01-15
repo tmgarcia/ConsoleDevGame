11eadcced6de5ff56c7a57654575717dda872639
Dale Chetney <dalechetney@gmail.com>
2015-01-15 01:02:00 -0700

    Mimicry part 3

--- DragonUnleashed/Assets/Scripts/Mimic.cs
+++ DragonUnleashed/Assets/Scripts/Mimic.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Mimic : MonoBehaviour {
 
-    public Mesh[] disguises;
+    public GameObject[] disguises;
 
 	// Use this for initialization
 	void Start () {
@@ -17,7 +17,33 @@ public class Mimic : MonoBehaviour {
 
     void BecomeAnotherPerson()
     {
-        if(disguises.Length>0)
-        gameObject.GetComponent<MeshFilter>().mesh = (Mesh)disguises[Random.Range(0, disguises.Length)];
+        if (disguises.Length > 0)
+        {
+            int i = Random.Range(0, disguises.Length);
+            GameObject temp = (GameObject)Instantiate(disguises[i],new Vector3(0,-50,0), new Quaternion());
+            CopyObject(temp.transform.GetChild(0).gameObject);
+            Destroy(temp);
+        }
+    }
+
+    private void CopyObject(GameObject other)
+    {
+        gameObject.GetComponent<MeshFilter>().mesh = other.GetComponent<MeshFilter>().mesh;
+
+        gameObject.renderer.material = other.renderer.material;
+        
+        Destroy(gameObject.collider);
+        if(other.collider.GetType()==typeof(BoxCollider))
+        {
+            gameObject.AddComponent<BoxCollider>();
+        }
+        else if(other.collider.GetType()==typeof(CapsuleCollider))
+        {
+            gameObject.AddComponent<CapsuleCollider>();
+        }
+        else if (other.collider.GetType() == typeof(SphereCollider))
+        {
+            gameObject.AddComponent<SphereCollider>();
+        }
     }
 }
