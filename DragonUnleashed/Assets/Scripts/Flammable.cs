cf30dbc5f921a2321ed4e1f6eebdd3a132c3bcca
Tyler Dacus <tdacus@student.neumont.edu>
2015-01-15 02:40:40 -0700

    :fire::fire:Rough fire implementation:fire::fire:

--- DragonUnleashed/Assets/Scripts/Flammable.cs
+++ DragonUnleashed/Assets/Scripts/Flammable.cs
@@ -4,30 +4,84 @@ using System.Collections;
 public class Flammable : Damageable
 {
 	private float BurninationLevel;
+    public ParticleSystem fire;
+    private ParticleSystem personalFire;
+    
 
 	void Start()
 	{
 		CurrentIntegrity = StartingIntegrity;
+
+
+        //reposition fire
+        Vector3 firePosition = gameObject.collider.bounds.center + new Vector3(0, gameObject.collider.bounds.size.y / 2, 0);      // Places fire on top of object
+        //Vector3 firePosition = gameObject.renderer.bounds.center;                                                                   // Places fire in center of object
+
+        personalFire = Instantiate(fire, firePosition, Quaternion.identity) as ParticleSystem;
+        personalFire.transform.Rotate(new Vector3(1, 0, 0), -90);
+
+        //resize fire 
+       
+        float longSideLength = Mathf.Max(gameObject.collider.bounds.size.x, gameObject.collider.bounds.size.z);
+        personalFire.transform.localScale *= longSideLength;
+
+        personalFire.emissionRate = 20 * longSideLength;
+
+        
+
+
+
+
+
+
+
 	}
 
 	void Update()
 	{
-		if (BurninationLevel > 0)
-		{
-			ReduceIntegrity();
-			ReduceBurnination();
-		}
+        //AdjustFlameParticles();
+        if (BurninationLevel > 0.5)/////////////////////////////////////////////////////////////////////////////////////Test lower bound
+        {
+            AdjustFlameParticles();
+            ReduceIntegrity();
+            ReduceBurnination();
+        }
+        else if(personalFire.isPlaying)
+        {
+            DisableFlameParticles();
+
+        }
 	}
+
+    private void AdjustFlameParticles()
+    {
+        if (personalFire.isPaused)
+        {
+            EnableFlameParticles();
+        }
+        personalFire.emissionRate = BurninationLevel * 20;
+        print(personalFire.emissionRate);
+    }
+
+    private void EnableFlameParticles()
+    {
+        personalFire.Play();
+    }
+
+    private void DisableFlameParticles()
+    {
+        personalFire.Pause();
+    }
 
 	private void ReduceBurnination()
 	{
-		BurninationLevel -= (BurninationLevel * Time.deltaTime);
+		BurninationLevel -= (BurninationLevel * Time.deltaTime);//////////////////////////////////////////////////Slow this down?
 	}
 
 	private void ReduceIntegrity()
 	{
 		CurrentIntegrity -= (BurninationLevel * Time.deltaTime);
-		print("CurrentIntegrity: " + CurrentIntegrity);
+		//print("CurrentIntegrity: " + CurrentIntegrity);
 		if (CurrentIntegrity <= 0)
 		{
 			Disintegrate();
@@ -49,5 +103,8 @@ public class Flammable : Damageable
 		{
 			Destroy(this.gameObject);
 		}
+        Destroy(personalFire, 2.0f);
+        BurninationLevel = 0;
+
 	}
 }
