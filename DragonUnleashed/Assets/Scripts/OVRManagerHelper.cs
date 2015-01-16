a7af92cc2cc4109eeef8e0dcad2784570996a39a
Jeremy Jones <jeremy29229@gmail.com>
2015-01-16 10:52:06 -0700

    Oculus only appears when plugged in
    TODO: allow gui to show on OVR, add ovr cam to be child on dragon head

---
+++ DragonUnleashed/Assets/Scripts/OVRManagerHelper.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class OVRManagerHelper : MonoBehaviour
+{
+	public bool IsLocalPlayerUsingOVR;
+	public static OVRManagerHelper instance;
+
+	void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+		}
+
+		IsLocalPlayerUsingOVR = IsOculusConnected();
+		print("Found OVR? " + IsLocalPlayerUsingOVR);
+
+		if (!IsLocalPlayerUsingOVR)
+		{
+			gameObject.SetActive(false);
+		}
+	}
+
+	void Start ()
+	{
+		
+	}
+
+	void Update ()
+	{
+	
+	}
+
+	private bool IsOculusConnected()
+	{
+		return Ovr.Hmd.Detect() > 0;
+	}
+
+	////////////FUTURE IMPLEMENTATION/////////////
+	//public boolCheckOculusConnection();
+	//print("connection: " + (Ovr.Hmd.Detect() > 0));
+	//OVRManager.HMDAcquired += CheckOculusConnection;
+	//OVRManager.HMDLost += CheckOculusConnection;
+}
