1e84b97d49059ef52fd8e17f69d277e287d47643
Jeremy Jones <jeremy29229@gmail.com>
2015-01-26 09:09:09 -0700

    Added the bake :balloon:

---
+++ DragonUnleashed/Assets/Scripts/MonsterMovement.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MonsterMovement : MonoBehaviour
+{
+	GameObject anchor;
+	GameObject player;
+	NavMeshAgent monsterMesh;
+	public float minMoveDisance = 1.0f;
+	public float maxMoveDistance = 5.0f;
+	public float maxAnchorDistance = 5.0f;
+	bool inRange = false;
+
+	//for dragon
+	private static Vector2 herdDirection;
+	private static float herdX;
+	private static float herdY;
+	private static bool isHerdSelectingDirection;
+
+	void Start()
+	{
+		monsterMesh = GetComponent<NavMeshAgent>();
+		anchor = GameObject.Find("DingoSpawner");
+		player = GameObject.Find("Player");
+	}
+
+	void Update()
+	{
+		if (inRange)
+		{
+			SeekPlayer();
+		}
+		else
+		{
+			if (monsterMesh.remainingDistance <= float.Epsilon)
+			{
+				PickTarget();
+			}
+		}
+	}
+
+	void PickTarget()
+	{
+		Vector2 direction = Random.insideUnitCircle;
+
+		Vector3 potentialTarget = transform.position + new Vector3(direction.x * Random.Range(minMoveDisance, maxMoveDistance), transform.position.y, direction.y * Random.Range(minMoveDisance, maxMoveDistance));
+
+		if (Vector3.Distance(potentialTarget, anchor.transform.position) > maxAnchorDistance)
+		{
+			//print("Failed to pick valid target");
+		}
+		else
+		{
+			monsterMesh.SetDestination(potentialTarget);
+		}
+	}
+
+	void SeekPlayer()
+	{
+		if (Vector3.Distance(player.transform.position, anchor.transform.position) <= maxAnchorDistance)
+		{
+			monsterMesh.SetDestination(player.transform.position);
+		}
+	}
+
+	void OnTriggerEnter(Collider c)
+	{
+		if (c.gameObject != null && c.gameObject == player)
+		{
+			inRange = true;
+		}
+	}
+
+	void OnTriggerExit(Collider c)
+	{
+		if (c.gameObject != null && c.gameObject == player)
+		{
+			inRange = false;
+		}
+	}
+}
---
+++ DragonUnleashed/Assets/Scripts/Spawner.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Spawner : MonoBehaviour
+{
+	public float spawnRadius = 30.0f;
+	public int maxSpawned = 10;
+	public int currentSpawned = 0;
+	public float spawnInterval = 1.0f;
+	private float currentSpawnInterval = 0.0f;
+	private GameObject dingo;
+
+	void Start()
+	{
+		dingo = GameObject.Find("jcDingo");
+		for (int i = 0; i < maxSpawned; i++)
+		{
+			Instantiate(dingo, new Vector3(gameObject.transform.position.x + (Random.insideUnitCircle.x * Random.Range(0, 30)), gameObject.transform.position.y, gameObject.transform.position.z + (Random.insideUnitCircle.y * Random.Range(0, 30))), Quaternion.identity);
+			currentSpawned++;
+		}
+		dingo.SetActive(false);
+	}
+
+	void Update()
+	{
+		if (currentSpawned < maxSpawned)
+		{
+			currentSpawnInterval += Time.deltaTime;
+
+			if (currentSpawnInterval >= spawnInterval)
+			{
+				currentSpawnInterval = 0.0f;
+				dingo.SetActive(true);
+
+				Instantiate(dingo, new Vector3(gameObject.transform.position.x + (Random.insideUnitCircle.x * Random.Range(0, 30)), gameObject.transform.position.y, gameObject.transform.position.z + (Random.insideUnitCircle.y * Random.Range(0, 30))), Quaternion.identity);
+
+				currentSpawned++;
+
+				dingo.SetActive(false);
+			}
+		}
+	}
+}
