f4fead09b73e41485a5f24b0397c122400a539bd
Taylor Garcia <tclustuff@gmail.com>
2015-01-26 10:15:02 -0700

    Fixed prop meshes & dragon camera is main camera

--- DragonUnleashed/Assets/Scripts/Mimic.cs
+++ DragonUnleashed/Assets/Scripts/Mimic.cs
@@ -53,27 +53,55 @@ public class Mimic : MonoBehaviour {
 
         gameObject.renderer.material = other.renderer.material;
         
-        Destroy(gameObject.collider);
+        DestroyImmediate(gameObject.collider);
+
+        print("Other prop collider:");
+        print("\tType: " + other.collider.GetType().ToString());
+
         if(other.collider.GetType()==typeof(BoxCollider))
         {
+            print("\tCenter: " + other.GetComponent<BoxCollider>().center.ToString());
+            print("\tSize: " + other.GetComponent<BoxCollider>().size.ToString());
             gameObject.AddComponent<BoxCollider>();
             gameObject.GetComponent<BoxCollider>().center = other.GetComponent<BoxCollider>().center;
             gameObject.GetComponent<BoxCollider>().size = other.GetComponent<BoxCollider>().size;
+
+            print("This prop collider:");
+            print("\tType: " + gameObject.collider.GetType().ToString());
+            print("\tCenter: " + gameObject.GetComponent<BoxCollider>().center.ToString());
+            print("\tSize: " + gameObject.GetComponent<BoxCollider>().size.ToString());
         }
         else if(other.collider.GetType()==typeof(CapsuleCollider))
         {
+            print("\tCenter: " + other.GetComponent<CapsuleCollider>().center.ToString());
+            print("\tRadius: " + other.GetComponent<CapsuleCollider>().radius.ToString());
+            print("\tHeight: " + other.GetComponent<CapsuleCollider>().height.ToString());
+            print("\tDirection: " + other.GetComponent<CapsuleCollider>().direction.ToString());
             gameObject.AddComponent<CapsuleCollider>();
             gameObject.GetComponent<CapsuleCollider>().center = other.GetComponent<CapsuleCollider>().center;
             gameObject.GetComponent<CapsuleCollider>().radius = other.GetComponent<CapsuleCollider>().radius;
             gameObject.GetComponent<CapsuleCollider>().height = other.GetComponent<CapsuleCollider>().height;
             gameObject.GetComponent<CapsuleCollider>().direction = other.GetComponent<CapsuleCollider>().direction;
+            print("This prop collider:");
+            print("\tType: " + gameObject.collider.GetType().ToString());
+            print("\tCenter: " + gameObject.GetComponent<CapsuleCollider>().center.ToString());
+            print("\tRadius: " + gameObject.GetComponent<CapsuleCollider>().radius.ToString());
+            print("\tHeight: " + gameObject.GetComponent<CapsuleCollider>().height.ToString());
+            print("\tDirection: " + gameObject.GetComponent<CapsuleCollider>().direction.ToString());
         }
         else if (other.collider.GetType() == typeof(SphereCollider))
         {
+            print("\tCenter: " + other.GetComponent<SphereCollider>().center.ToString());
+            print("\tRadius: " + other.GetComponent<SphereCollider>().radius.ToString());
             gameObject.AddComponent<SphereCollider>();
             gameObject.GetComponent<SphereCollider>().center = other.GetComponent<SphereCollider>().center;
             gameObject.GetComponent<SphereCollider>().radius = other.GetComponent<SphereCollider>().radius;
+            print("This prop collider:");
+            print("\tType: " + gameObject.collider.GetType().ToString());
+            print("\tCenter: " + gameObject.GetComponent<SphereCollider>().center.ToString());
+            print("\tRadius: " + gameObject.GetComponent<SphereCollider>().radius.ToString());
         }
+        
         Destroy(other);
     }
 }
