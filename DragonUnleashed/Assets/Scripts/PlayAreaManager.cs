edbf48c046e6873b08793ca1511f6f03af6ce326
Jeremy Jones <jeremy29229@gmail.com>
2015-01-27 10:09:52 -0700

    Organized assets and added play area

---
+++ DragonUnleashed/Assets/Scripts/PlayAreaManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class PlayAreaManager : MonoBehaviour
+{
+	void Start ()
+	{
+	
+	}
+
+	void Update ()
+	{
+		//gameObject.GetComponent<Material>().SetTextureOffset(gameObject.GetComponent<Material>().GetTextureOffset() + Time.deltaTime);
+		//renderer.material.SetTextureOffset("PlayAreaLines", renderer.material.GetTextureOffset("PlayAreaLines") + new Vector2(Time.deltaTime, Time.deltaTime));
+		renderer.material.mainTextureOffset += new Vector2(Time.deltaTime / 5, Time.deltaTime / 5);
+	}
+}
