f0c28347dec6849b97a07a4989267e78f3c98bf4
Dale Chetney <dalechetney@gmail.com>
2015-01-29 23:13:37 -0700

    W-W-W-WINDMILL!!

---
+++ DragonUnleashed/Assets/Scripts/Windmill.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Windmill : MonoBehaviour {
+    private Vector3 axis = new Vector3(0, 0, 1);
+    public float speed = 2.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(axis, -speed);
+	}
+}
