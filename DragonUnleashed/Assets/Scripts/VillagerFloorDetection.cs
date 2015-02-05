533989c3345175438469b56beed8a50c7cfc196d
Luis Andrade <landrade1093@gmail.com>
2015-02-05 10:52:17 -0700

    Jumping improved

--- DragonUnleashed/Assets/Scripts/VillagerFloorDetection.cs
+++ DragonUnleashed/Assets/Scripts/VillagerFloorDetection.cs
@@ -12,6 +12,23 @@ public class VillagerFloorDetection : MonoBehaviour
         Physics.IgnoreCollision(collider, villager.collider);
 	}
 
+    void LateUpdate()
+    {
+        MeshFilter filter = transform.parent.GetComponent<MeshFilter>();
+        string meshName = filter.mesh.ToString();
+        MeshRenderer parentRenderer = transform.parent.GetComponent<MeshRenderer>();
+        if (meshName.Contains("default"))
+        {
+            float halfHeight = parentRenderer.bounds.size.y / 2.0f;
+            transform.localPosition = new Vector3(transform.localPosition.x, -(halfHeight + 0.05f), transform.localPosition.z);
+        }
+        else
+        {
+            float distance = parentRenderer.bounds.size.y / 5.0f;
+            transform.localPosition = new Vector3(transform.localPosition.x, -(distance - 0.1f), transform.localPosition.z);
+        }
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.tag != "Player")
