7819eb44fdcc5785b3afc7043f2ac902041c803b
Taylor Garcia <tclustuff@gmail.com>
2015-02-06 09:11:31 -0700

    Fixed mimic controls to local only

--- DragonUnleashed/Assets/Scripts/Mimic.cs
+++ DragonUnleashed/Assets/Scripts/Mimic.cs
@@ -16,18 +16,21 @@ public class Mimic : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        
-        if (Input.GetKeyDown(KeyCode.E)&&!Input.GetKey(KeyCode.Mouse1)) MimicProp();
-        if (currentDisguise != "Villager" && Input.GetKeyDown(KeyCode.Mouse1))
+	void Update () 
+    {
+        if (GetComponent<Player>().IsLocal)
         {
-            gameObject.GetComponent<PhotonView>().RPC("SwitchModels", PhotonTargets.All);
-            gameObject.GetComponent<PhotonView>().RPC("CopyObject", PhotonTargets.All, "Villager");
-        }
-        if (currentDisguise != "Villager" && Input.GetKeyUp(KeyCode.Mouse1))
-        {
-            gameObject.GetComponent<PhotonView>().RPC("SwitchModels", PhotonTargets.All);
-            gameObject.GetComponent<PhotonView>().RPC("CopyObject", PhotonTargets.All,currentDisguise);
+            if (Input.GetKeyDown(KeyCode.E) && !Input.GetKey(KeyCode.Mouse1)) MimicProp();
+            if (currentDisguise != "Villager" && Input.GetKeyDown(KeyCode.Mouse1))
+            {
+                gameObject.GetComponent<PhotonView>().RPC("SwitchModels", PhotonTargets.All);
+                gameObject.GetComponent<PhotonView>().RPC("CopyObject", PhotonTargets.All, "Villager");
+            }
+            if (currentDisguise != "Villager" && Input.GetKeyUp(KeyCode.Mouse1))
+            {
+                gameObject.GetComponent<PhotonView>().RPC("SwitchModels", PhotonTargets.All);
+                gameObject.GetComponent<PhotonView>().RPC("CopyObject", PhotonTargets.All, currentDisguise);
+            }
         }
 	}
 
